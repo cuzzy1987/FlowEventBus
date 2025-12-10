c5ac9616b900c1674dcf67256f5a489cc6c9553a
cuzzy <cuzzy@weihai.com>
2025-12-10 14:52:07 +0800

    change: alternative compile version and java version

--- FlowEventBusLib/build.gradle.kts
+++ FlowEventBusLib/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.cuzzy.floweventbuslib"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 24
@@ -16,11 +16,11 @@ android {
 
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
